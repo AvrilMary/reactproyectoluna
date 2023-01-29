2320fe54f2edda3a9cd5f37bcf7945065df8c201
Avril_M <turvey.am@gmail.com>
2023-01-28 22:23:53 -0600

    Added English/spanish translations

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 
 import './App.css';
 import React from 'react';
+import "./i18n";
+import LanguageSelector from "./components/LanguageSelector";
 import Home from './components/Home';
 import NuestraHistoria from './components/NuestraHistoria';
 import {Route, Routes} from 'react-router-dom';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
+
+      <LanguageSelector />
+  
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/nuestraHistoria/" element={<NuestraHistoria/>}/>
       </Routes>
+    
+
     </div>
   );
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,24 +1,27 @@
 import React from "react";
+import { withTranslation } from "react-i18next";
 
-export default function Form () {
+
+const Form =  ({t}) => {
+
     return (
 
     <aside>
       <div class="content-wrapper">
         <div class="form-description">
-          <h3>¿Estas interesada en participar en nuestro próximo Retreat?
+          <h3>  {t("formTitle.label")}
           </h3>
-          <p>Para recibir más información, escribe tu correo abajo.
+          <p>{t("formDescription.label")}
           </p>
         </div>
         <div class="form">
           <input type="text" name="name" value="Correo electronico"/>
-          <input type="button" value="Recibir guía"/>
+          <input type="button" value={t("formButton.label")}/>
         </div>
       </div>
     </aside>
-
-
     )
 
 }
+
+export default withTranslation()(Form);
