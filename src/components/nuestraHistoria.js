43d5139396573f04b7f9b763924384d70ddb0d15
Avril_M <turvey.am@gmail.com>
2023-02-05 22:29:57 -0600

    Added nuestra historia page

---
+++ src/components/nuestraHistoria.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./Nav";
+import Footer from "./Footer";
+
+
+export default function NuestraHistoria () {
+    return (
+        <>
+         <NavBar/>
+         <Footer/>
+        </>
+    )
+}
