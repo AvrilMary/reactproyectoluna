f77ce98810e5259414be6126a732587c507c13d4
Avril_M <turvey.am@gmail.com>
2023-02-06 22:00:58 -0600

    Added new contact form

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -1,8 +1,123 @@
-import React from "react";
 import NavBar from "./Nav";
 import Footer from "./Footer";
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 
-class Contacto extends React.Component {
+
+const Contacto = (props) => {
+  const [mailSent, setmailSent] = useState(false);
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+/**
+  * @function handleFormSubmit
+  * @param e { obj } - form event
+  * @return void
+  */
+
+ const handleFormSubmit = e => {
+  e.preventDefault();
+  axios({
+    method: "post",
+    url: `${process.env.REACT_APP_API}`,
+    headers: { "content-type": "application/json" },
+    data: formData
+  })
+    .then(result => {
+      if (result.data.sent) {
+        setmailSent(result.data.sent)
+        setError(false)
+      } else {
+        setError(true)
+      }
+    })
+    .catch(error => setError( error.message ));
+};
+/**
+  * @function handleChange
+  * @param e { obj } - change event
+  * @param field { string } - namve of the field
+  * @return void
+  */
+ const handleChange = (e, field) => {
+  let value = e.target.value;
+  setFormData({
+    ...formData,
+    [field]: value,
+  });
+};
+
+
+
+  const { title,description, successMessage, 
+    errorMessage, fieldsConfig } = props.config;
+
+    return (
+      <>
+      <NavBar/>
+      <div className="contact-form">
+        <div className="contact-form__header">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+      <div className="contact-form__container">
+        <div>
+          <form action="#">
+            {fieldsConfig &&
+              fieldsConfig.map(field => {
+                return (
+                  <React.Fragment key={field.id}>
+                    {field.type !== "textarea" ? (
+                      <React.Fragment>
+                        <label>{field.label}</label>
+                        <input
+                          type={field.type}
+                          className={field.klassName}
+                          placeholder={field.placeholder}
+                          value={field.name}
+                          onChange={e => handleChange(e, field.fieldName)}
+                        />
+                      </React.Fragment>
+                    ) : (
+                      <React.Fragment>
+                        <label>{field.label}</label>
+                        <textarea className={field.klassName} placeholder={field.placeholder} onChange={e => handleChange(e, field.fieldName)} value={field.name} />
+                      </React.Fragment>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            <input type="submit" onClick={e => handleFormSubmit(e)} value="Submit" />
+            <div>
+              {mailSent && <div className="sucsess">{successMessage}</div>}
+              {error && <div className="error">{errorMessage}</div>}
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
+      <Footer/>
+      </>
+    );
+
+
+}
+
+export default Contacto;
+
+Contacto.propTypes = {
+  config: PropTypes.object.isRequired
+};
+
+
+
+
+
+
+
+
+/*class Contacto extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
@@ -78,18 +193,6 @@ class Contacto extends React.Component {
     }
   }
   export default Contacto;
-
-/*
-<>
-<NavBar/>
-       <div class="form-description">
-           <h3>Contacto</h3>
-           <h2 class="subtitles">¡Escribirnos!</h2>
-               <p class="contactDescription">Una pregunta o solicitud sobre los Retreats que ofrecemos...
-               </p>
-       </div>
+  */
 
 
-<Footer/>
-</>
-*/
