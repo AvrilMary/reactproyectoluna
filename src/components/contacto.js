b38e404ecdd8876b2a89b7671741f2b153a16de3
Avril_M <turvey.am@gmail.com>
2023-02-08 21:22:48 -0600

    Continued working on form

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -8,12 +8,19 @@ export default function Contacto() {
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
+    reset
   } = useForm();
+
 
   const onSubmit = (data) => {
     console.log(data);
     console.log({ ...register("email") });
+    reset({
+      username: "Alex",
+      email: "alex@example.com",
+      password: "Test@123"
+  });
   };
 
   return (
@@ -23,11 +30,43 @@ export default function Contacto() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
           <label>Email</label>
-          <input type="text" name="email" {...register("email")} />
+          <input
+            type="text"
+            name="email"
+            {...register("email", {
+              required: true,
+              pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
+            })}
+          />
+           {errors.email && errors.email.type === "required" && (
+            <p className="errorMsg">Email is required.</p>
+          )}
+          {errors.email && errors.email.type === "pattern" && (
+            <p className="errorMsg">Email is not valid.</p>
+          )}
+
         </div>
         <div className="form-control">
           <label>Password</label>
-          <input type="password" name="password" {...register("password")} />
+          <input
+            type="password"
+            name="password"
+            {...register("password", {
+              required: true,
+              minLength: 6
+            })}
+          />
+          {errors.password && errors.password.type === "required" && (
+            <p className="errorMsg">Password is required.</p>
+          )}
+          {errors.password && errors.password.type === "minLength" && (
+            <p className="errorMsg">
+              Password should be at-least 6 characters.
+            </p>
+          )}
+
+
+
         </div>
         <div className="form-control">
           <label></label>
